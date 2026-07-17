48ae8b5bbe258c13b1e07afae71588d2c68e7224
Roman D <rmd-practicum@iamroman.org>
2026-07-17 19:28:45 +0300

    Task 2: simplify networking.

--- mongo-sharding/mongo-init.sh
+++ mongo-sharding/mongo-init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker compose exec -T configSrv mongosh --port 27017 <<EOF
+docker compose exec -T configSrv mongosh --port 27019 <<EOF
 rs.initiate(
   {
     _id : "config_server",
     configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27017" }
+      { _id : 0, host : "configSrv:27019" }
     ]
   }
 );
@@ -25,12 +25,12 @@ rs.initiate(
 EOF
 
 
-docker compose exec -T shard2 mongosh --port 27019 <<EOF
+docker compose exec -T shard2 mongosh --port 27018 <<EOF
 rs.initiate(
   {
     _id : "shard2",
     members: [
-      { _id : 0, host : "shard2:27019" },
+      { _id : 0, host : "shard2:27018" },
     ]
   }
 );
@@ -39,9 +39,9 @@ EOF
 
 sleep 3
 
-docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+docker compose exec -T mongos_router mongosh <<EOF
 sh.addShard("shard1/shard1:27018");
-sh.addShard("shard2/shard2:27019");
+sh.addShard("shard2/shard2:27018");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb
